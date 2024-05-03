ac3ab851cc4a11ff64313a9ea7e8d2b8f9a5fd0f
ruu <wjrdjs005@naver.com>
2024-05-03 17:56:32 +0900

    🐛 fix: 댓글 작성자 나오지않는 문제 수정

--- src/main/java/org/ruu/developerkorea/domain/board/model/dto/comment/ResponseCommentDTO.java
+++ src/main/java/org/ruu/developerkorea/domain/board/model/dto/comment/ResponseCommentDTO.java
@@ -1,10 +1,13 @@
 package org.ruu.developerkorea.domain.board.model.dto.comment;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.ruu.developerkorea.domain.board.domain.comment.Comment;
+
+import java.time.LocalDateTime;
 
 @Builder
 @Getter
@@ -18,11 +21,15 @@ public class ResponseCommentDTO {
 
     private String commentWriter;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updateAt;
+
     public static ResponseCommentDTO of(Comment comment) {
         return ResponseCommentDTO.builder()
                 .commentId(comment.getId())
                 .commentText(comment.getText())
                 .commentWriter(comment.getWriter())
+                .updateAt(comment.getUpdatedAt())
                 .build();
     }
 
